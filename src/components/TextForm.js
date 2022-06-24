940846b37ec444ad1b71df6dde2819b9f3e2a8d9
abdullah <abdullasif1>
2022-06-24 16:04:13 +0500

    Added more logics to my app

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,6 +3,11 @@ import React, {useState} from 'react'
 
 
 export default function TextForm(props) {
+    const handleLoClick = () => {
+        console.log("lowercase was clicked");
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
     const handleUpClick = ()=>{
         console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
@@ -21,12 +26,23 @@ export default function TextForm(props) {
   
   
     return (
+        <>
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="3"></textarea>
         </div>
-        <button className="btn btn primary"  onClick={handleUpClick}>Conversion to Upper case</button>
+        <button className= "btn btn primary mx-2"  onClick={handleUpClick}>Conversion to Upper case</button>
+        <button className= "btn btn-primary mx-2" onClick={handleLoClick}> Convert to Lowercase</button>
+
     </div>
+    <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008* text.split(" ").length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
