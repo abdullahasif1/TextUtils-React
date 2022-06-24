a822edcef01376687100b98dfcd90c423be7bf11
abdullah <abdullasif1>
2022-06-24 23:46:44 +0500

    Updates

---
+++ precache-manifest.25e47554a6194c8c5912826a7e19a751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cab22fbd2649ce12a3d4",
+    "url": "/TextUils-React/static/css/main.d6528b77.chunk.css"
+  },
+  {
+    "revision": "cab22fbd2649ce12a3d4",
+    "url": "/TextUils-React/static/js/main.cab22fbd.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUils-React/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/TextUils-React/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "4b13f11472de50b3d606",
+    "url": "/TextUils-React/static/js/runtime~main.4b13f114.js"
+  },
+  {
+    "revision": "fa382805526da1ff6096d166a604e621",
+    "url": "/TextUils-React/index.html"
+  }
+];
