b2ff44d4f0f71279c4cb5dec304245d5ef6ca69b
abdullah <abdullasif1>
2022-06-25 00:08:00 +0500

    Updates

---
+++ precache-manifest.462e6adfc5df8edf4960e57c5565c3bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cab22fbd2649ce12a3d4",
+    "url": "/TextUtils-React/static/css/main.d6528b77.chunk.css"
+  },
+  {
+    "revision": "cab22fbd2649ce12a3d4",
+    "url": "/TextUtils-React/static/js/main.cab22fbd.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils-React/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/TextUtils-React/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "0d79c3c2a5762a692cdb",
+    "url": "/TextUtils-React/static/js/runtime~main.0d79c3c2.js"
+  },
+  {
+    "revision": "26bcf852e8265a4644936e53683122ab",
+    "url": "/TextUtils-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TextUils-React/precache-manifest.25e47554a6194c8c5912826a7e19a751.js"
+  "/TextUtils-React/precache-manifest.462e6adfc5df8edf4960e57c5565c3bb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/TextUils-React/index.html", {
+workbox.routing.registerNavigationRoute("/TextUtils-React/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
