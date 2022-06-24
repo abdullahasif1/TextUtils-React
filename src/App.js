0397d3f8f985c2aade9e629292e5ffe0e7503f84
abdullah <abdullasif1>
2022-06-24 17:47:37 +0500

    Enable Dark Mode options

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import About from './components/About';
 import NavBar from './components/NavBar';
-import TextForm from './components/TextForm';
+//import TextForm from './components/TextForm';
 function App() {
   return (
     ///JSX Fragment
   <>   
       <NavBar title="Menu" dropdown="About website"/>
-      <div className="container">
+      <div className="container my-3">
        
-        <TextForm heading="Enter the text to moniter changes"/> 
+      {/*  <TextForm heading="Enter the text to moniter changes"/> */}
+      <About/>
       </div>
        
   </>
